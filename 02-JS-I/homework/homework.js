e3115ab7845fab18dbf4f77e8a302a26378cf711
Lucas Alfonzo <lucasalfonzo19@gmail.com>
2021-11-05 15:47:48 -0300

    Finished Homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -257,7 +257,7 @@ function esVocal(letra){
  if (letra.length > 1){
    return "Dato incorrecto"
  }
- if (letra === "a" || letra === "e" || letra === "i" || letra === "e" || letra === "o" || letra === "u"){
+ if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
     return "Es vocal"
  }
  return "Dato incorrecto"
